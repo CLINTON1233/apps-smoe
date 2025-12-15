310eb68768cd25973751cb3b4c9949fbfaae7e09
CLINTON1233 <PROJECT SIALAN 12>
2025-12-15 15:46:52 +0700

    ftp

--- src/config/api.js
+++ src/config/api.js
@@ -41,6 +41,11 @@ export const SMOE_API = {
 export const API_ENDPOINTS = {
   ...PORTAL_API,
   ...SMOE_API,
+    APPLICATION_DOWNLOAD: (id) => `${SMOE_API_URL}/applications/${id}/ftp-download`,
+  
+  // Endpoint untuk test FTP
+  FTP_TEST: `${SMOE_API_URL}/ftp/test`,
+
 };
 
 // Helper functions
@@ -71,6 +76,7 @@ export const getPortalLoginUrl = (returnUrl = null) => {
   }
   return PORTAL.LOGIN_PAGE;
 };
+
 
 export default {
   PORTAL,
